efd5d6fbe26c80a311097e04014d2920199f1563
numbers0580 <koreankid18@yahoo.com>
2020-06-12 11:17:04 -0600

    Even reviewing all of my notes, I'm having difficulty with maintaining objects fetched from axios. My tests seems to successfully get the data, but then drops the data when I need to use it.

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Character from './components/Character';
 import './App.css';
 
 const App = () => {
@@ -12,6 +13,7 @@ const App = () => {
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      <Character />
     </div>
   );
 }
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,51 @@
 // Write your Character component here
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+
+const Character = props => {
+    const [rmData, updateRM] = useState();
+    
+    useEffect(() => {
+        axios.get('https://rickandmortyapi.com/api/character/1,2,3,7,8,14,24,26,40,47,150,244,265,372')
+            .then(portalGun => {
+                debugger
+                updateRM(portalGun.data);
+                debugger
+                console.log(portalGun.data.name);
+                //console.log(rmData);
+                debugger
+                //console.log(rmData.location.name);
+            })
+            .catch(badPortal => {
+                console.log('Oops! We just teleported in to the Galactic Federation');
+            })
+    }, []);
+
+    debugger;
+    console.log(rmData);
+    /*
+        rmData now returns:
+        0: {id: 1, name: "Rick Sanchez", status: "Alive", etc}
+        .
+        .
+        .
+        13 {id, name, status, species, episode{}, location{}, origin{}, etc}
+    */
+    debugger;
+    // After trying to create the return elements though, I'm getting errors
+
+    rmData.map(function(rmObj, rmIndex) {
+        return (
+            // Create character info cards here using .map
+            <div>
+                <div>
+                    <img src={rmObj.image} />
+                    <h3>{rmObj.name}</h3>
+                </div>
+                <p>Species: {rmObj.species}</p>
+            </div>
+        );
+    })
+};
+
+export default Character;
