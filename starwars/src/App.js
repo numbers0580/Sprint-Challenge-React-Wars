08d08bb09f50ed35cf51220c79768758a74d3730
numbers0580 <koreankid18@yahoo.com>
2020-06-12 15:22:40 -0600

    Almost there. Going through final checks for minor tweaks.

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -9,15 +9,13 @@ const App = () => {
 
   //Haven't tested yet, but I want to try pulling data from CharacterPage, first
   //Nope, that attempt failed again. Uncommenting code below and will try this.
-  const [rmContent, rmUpdate] = useState();
+  const [rmContent, rmUpdate] = useState([]);
   useEffect(() => {
     axios.get('https://rickandmortyapi.com/api/character/1,2,3,7,8,14,24,26,40,47,150,244,265,372')
       .then(fetchedRM => {
-        debugger
         console.log('fetchedRM.data is ', fetchedRM.data);
         let passedRM = fetchedRM.data;
         rmUpdate(passedRM);
-        debugger
         console.log('passedRM is ', passedRM);
         console.log('rmContent is ', rmContent);
       })
@@ -34,20 +32,22 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
-  debugger
-  let tempVar = rmContent;
-  console.log('tempVar is ', tempVar);
-  debugger
+  let rmPkg = rmContent;
+  console.log('tempVar is ', rmPkg);
 
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
-      {/*
-        tempVar.map(function(tempObj, tempIndex) {
-          return '';
-        })
-      */}
-      <CharacterPage rmData={tempVar} />
+      <header>
+        <img src="https://upload.wikimedia.org/wikipedia/en/c/c8/Rick_and_Morty_logo.png" alt="Rick and Morty Logo" />
+        <h1 className="Header">Characters</h1>
+      </header>
+      <section className="displayRM">
+        {
+          rmPkg.map(function(rmItem, rmPos) {
+            return <CharacterPage rmData={rmItem} rmKey={rmPos} />;
+          })
+        }
+      </section>
     </div>
   );
 }
--- starwars/src/components/CharacterPage.js
+++ starwars/src/components/CharacterPage.js
@@ -1,12 +1,12 @@
 // Write your Character component here
 import React, {useState, useEffect} from 'react';
-import Character from './Character';
-import axios from 'axios';
+//import Character from './Character';
+
 
 const CharacterPage = props => {
 
     //const [rmData, updateRM] = useState(props);
-    const {rmData} = props;
+    const {rmData, rmKey} = props; //The above commented-out line didn't work, but found this method worked well
     
     /*
     useEffect(() => {
@@ -27,9 +27,12 @@ const CharacterPage = props => {
     }, []);
     */
 
-    debugger;
     let rmHolder = rmData;
-    console.log(rmData);
+    let keyHolder = rmKey;
+    console.log('rmHolder is ', rmHolder); //Testing. Found rmData returned undefined, but rmHolder held the object
+    console.log('Current key is ', keyHolder); //Testing. See notes in the above line
+    console.log('Current Character Name: ', rmHolder.name); //Testing: Just to be sure.
+
     /*
         rmData now returns:
         0: {id: 1, name: "Rick Sanchez", status: "Alive", etc}
@@ -37,39 +40,67 @@ const CharacterPage = props => {
         .
         .
         13 {id, name, status, species, episode{}, location{}, origin{}, etc}
+
+        Update: I had to store everything from axios into new variables: rmHolder & key Holder, but making progress again
     */
-    debugger;
-    // After trying to create the return elements though, I'm getting errors
+
+    //This took a while, but I had originally tried using axios above and using .map in the return below, but I was getting errors that I couldn't fix
+    //Ultimately, I used axios in App.js, stored the fetched array into a new variable declared outside of the axios function and mapped over that new variable
+    //with the return statement in App.js to send each one to this page. Thus, there was no longer any need to use .map here
+
+
+    //Styling
+    const citadelOfRicks = function() {
+        return {
+            mainDiv: {
+                width: '1000px',
+                display: 'flex',
+                justifyContent: 'space-between',
+                alignItems: 'center',
+                padding: '20px',
+                borderRadius: '10px',
+                backgroundColor: '#97C12F',
+                boxShadow: '0 5px 30px -10px yellow',
+                marginBottom: '20px'
+            },
+
+            nestedDiv: {
+                display: 'flex',
+                alignItems: 'center'
+            },
+
+            img: {
+                width: '100px',
+                height: '100px',
+                marginRight: '20px'
+            },
+
+            h2: {
+                color: '298B38'
+            },
+
+            p: {
+                color: 'black'
+            },
+
+            span: {
+                fontWeight: 'bold',
+                color: 'red'
+            }
+        }
+    };
+
+
 
     return (
-        <div>
-            {
-                rmData.map(function(rmObj, rmIndex) {
-                    return <Character key={rmIndex} rmPost={rmObj} />;
-                })
-            }
-            {/*
-                rmData.foreach(function(rmObj, rmIndex) {
-                    return <Character key={rmIndex} rmPost={rmObj} />;
-                })
-            */}
+        <div style={citadelOfRicks().mainDiv}>
+            <div style={citadelOfRicks().nestedDiv}>
+                <img src={rmHolder.image} style={citadelOfRicks().img} />
+                <h2 style={citadelOfRicks().h2}>{rmHolder.name}</h2>
+            </div>
+            <p style={citadelOfRicks().p}>Location: <span style={citadelOfRicks().span}>{rmHolder.location.name}</span></p>
         </div>
     );
-
-    /* //The code below kept failing
-    rmData.map(function(rmObj, rmIndex) {
-        return (
-            // Create character info cards here using .map
-            <div>
-                <div>
-                    <img src={rmObj.image} />
-                    <h3>{rmObj.name}</h3>
-                </div>
-                <p>Species: {rmObj.species}</p>
-            </div>
-        );
-    })
-    */
 };
 
 export default CharacterPage;
